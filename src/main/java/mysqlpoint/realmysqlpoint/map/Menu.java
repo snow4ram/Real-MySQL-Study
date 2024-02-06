47a2d3e06338753821de4965b64d777f57756877
Joong Seok Hwang <hjs7949@naver.com>
2024-02-07 02:14:47 +0900

    feat: Restaurant menu
    레스토랑 메뉴 정보

---
+++ src/main/java/mysqlpoint/realmysqlpoint/map/Menu.java
@@ -0,0 +1,17 @@
+package mysqlpoint.realmysqlpoint.map;
+
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class Menu {
+
+    private String title;
+
+    private BigDecimal price;
+
+    private String body;
+
+}
