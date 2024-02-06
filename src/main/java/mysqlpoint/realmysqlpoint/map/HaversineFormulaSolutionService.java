4548b1e74ab84ed37f372ca0fd864534ebdb7da0
Joong Seok Hwang <hjs7949@naver.com>
2024-02-07 02:19:10 +0900

    feat: Generating polygon lines around the user using the Haversine formula.
    하버사인 공식을 통해 사용자 주변 폴리곤 라인 생성

--- src/main/java/mysqlpoint/realmysqlpoint/map/HaversineFormulaSolutionService.java
+++ src/main/java/mysqlpoint/realmysqlpoint/map/HaversineFormulaSolutionService.java
@@ -8,19 +8,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class HaversineFormulaSolutionService {
-        double latitude = 35.8389726;
-        double longitude = 128.7210818;
-        double radius = 10.0; // 반경 값 설정 (예: 5.0 km) mbr 반 지름 설정
-
-//        double latitude = 37.4901548250937;
-//        double longitude = 127.030767490957;
-//        double radius = 30.0; // 반경 값 설정 (예: 5.0 km) mbr
-
-        //37.55371926075816y1 127.11098061003622x2 37.42653625138507y2 126.95069083279373//111.321 : [128.6656765288934, 35.88388775527169, 128.7764870711066, 35.79405744472831]
-
-        //111.32 : [128.66567603118165, 35.88388815874955, 128.77648756881837, 35.79405704125045]
-
-        //LINESTRING(128.73494891328122 35.8502141447768,128.7072146867188 35.8277310552232)
 
     //todo  latitude : 위도 , longitude : 경도 , radius:  반지름
 
@@ -36,6 +23,8 @@ public class HaversineFormulaSolutionService {
 
         // 지구 반지름
         double angularDistance = travelDistance / 6378.1370;
+
+        log.info("방위각 = {} " , travelBearingRadians);
 
         // 구면 삼각법 공식을 사용하여 목적지 위도를 계산
         double destinationLatitudeRadians = Math.asin(
